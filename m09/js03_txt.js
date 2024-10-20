fa9f9cf4ddd20b5c15c3be9134d286714f0c5c29
Terry Allee <terryallee@westernwyoming.edu>
2024-10-20 15:54:47 -0600

    Add files via upload

--- m09/js03_txt.js
+++ m09/js03_txt.js
@@ -1,9 +1,11 @@
 /*
 	Tipton Turbines
      Program to display games results in a web table
-     Author:
-     Date:
+     Author: Terry Allee
+     Date: October 19, 2024
 
      Filename: js03.js
  */
+// Days of the week
+let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
